ba40c224bbe9febe3078895b4e682674ec3282d5
Gong Meng <18588264128@163.com>
2022-07-22 16:55:26 +0800

    web程序初步理解

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-package main
+package main // 入口包
 
 import (
 	"fmt"
@@ -8,6 +8,8 @@ import (
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 这里是 goblog</h1>")
 }
+
+// 入口函数
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
