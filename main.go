38413daff70042a280552fa3fcca4ac90423ff86
Gong Meng <18588264128@163.com>
2022-07-22 17:44:57 +0800

    尝试 http.ServeMux

--- main.go
+++ main.go
@@ -17,13 +17,41 @@ if r.URL.Path == "/" {
 }
 */
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// func handlerFunc(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	if r.URL.Path == "/" {
+// 		fmt.Fprint(w, "<h1>Hello, 这里是 goblog! </h1>")
+// 	} else if r.URL.Path == "/about" {
+// 		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+// 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+// 	} else {
+// 		w.WriteHeader(http.StatusNot)
+// 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+// 			"<p>如有疑惑，请联系我们。</p>")
+// 	}
+// }
+
+// func defaultHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	if r.URL.Path == "/" {
+// 			fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+// 	} else {
+// 			w.WriteHeader(http.StatusNotFound)
+// 			fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+// 					"<p>如有疑惑，请联系我们。</p>")
+// 	}
+// }
+
+// func aboutHandler(w http.ResponseWriter, r *http.Request) {
+// 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+// 	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+// 			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+// }
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello, 这里是 goblog! </h1>")
-	} else if r.URL.Path == "/about" {
-		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog</h1>")
 	} else {
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
@@ -31,8 +59,18 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+		"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+}
+
 // 入口函数
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	// 自定义路由
+	router := http.NewServeMux()
+	router.HandleFunc("/", defaultHandler)
+	router.HandleFunc("/about", aboutHandler)
+
+	http.ListenAndServe(":3000", router)
 }
