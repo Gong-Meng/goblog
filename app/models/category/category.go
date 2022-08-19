107d3f0b14cf37865904e8a1e847cebc7767ff50
Gong Meng <18588264128@163.com>
2022-08-19 16:38:55 +0800

    显示分类

--- app/models/category/category.go
+++ app/models/category/category.go
@@ -2,8 +2,7 @@ package category
 
 import (
 	"goblog/app/models"
-	"goblog/pkg/logger"
-	"goblog/pkg/model"
+	"goblog/pkg/route"
 )
 
 // Category 文章分类
@@ -13,12 +12,7 @@ type Category struct {
 	Name string `gorm:"type:varchar(255);not null;" valid:"name"`
 }
 
-// Create 创建分类, 通过 category.ID 来判断是否创建成功
-func (category *Category) Create() (err error) {
-	if err = model.DB.Create(&category).Error; err != nil {
-		logger.LogError(err)
-		return err
-	}
-
-	return nil
+// Link 方法用来生成文章链接
+func (category Category) Link() string {
+	return route.Name2URL("categories.show", "id", category.GetStringID())
 }
