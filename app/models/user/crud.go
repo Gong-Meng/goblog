8c8116e81afaf6f5ccea0dfb51835f525e6c51e7
Gong Meng <18588264128@163.com>
2022-08-19 15:17:08 +0800

    显示作者

--- app/models/user/crud.go
+++ app/models/user/crud.go
@@ -6,6 +6,15 @@ import (
 	"goblog/pkg/types"
 )
 
+// All 获取所有用户数据
+func All() ([]User, error) {
+	var users []User
+	if err := model.DB.Find(&users).Error; err != nil {
+		return users, err
+	}
+	return users, nil
+}
+
 // Create 创建用户，通过 User.ID 来判断是否创建成功
 func (user *User) Create() (err error) {
 	if err = model.DB.Create(&user).Error; err != nil {
